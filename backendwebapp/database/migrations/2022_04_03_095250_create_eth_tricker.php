883efb043541453d6c20adb842193d53af8940d2
steffen <stped18@student.sdu.dk>
2022-04-03 16:04:55 +0200

    first

---
+++ backendwebapp/database/migrations/2022_04_03_095250_create_eth_tricker.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eth_tricker', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->float("ask");
+            $table->float("bid");
+            $table->float("volume");
+            $table->float("price");
+            $table->float("size");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eth_tricker');
+    }
+};
